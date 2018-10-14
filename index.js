3e2ad95e856402b7bfd732d8d554c4625f4f41f4
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 02:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ function add2(n) {
   return n + 2
 }
 
-function funkyFunction() {
-    return "FUNKY!"
-}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
